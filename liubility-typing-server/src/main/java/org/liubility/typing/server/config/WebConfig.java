32da35c5635de3027ce1746146b7868546f69d2c
zhengjinlong <1061917196@qq.com>
2022-09-04 15:02:31 +0800

    feat: 跨域问题

--- liubility-typing-server/src/main/java/org/liubility/typing/server/config/WebConfig.java
+++ liubility-typing-server/src/main/java/org/liubility/typing/server/config/WebConfig.java
@@ -27,21 +27,21 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addWebRequestInterceptor(contextHolderInterceptor());
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig());
-        return new CorsFilter(source);
-    }
-
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 1允许任何域名使用
-        corsConfiguration.addAllowedOrigin("*");
-        // 2允许任何头
-        corsConfiguration.addAllowedHeader("*");
-        // 3允许任何方法（post、get等）
-        corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig());
+//        return new CorsFilter(source);
+//    }
+//
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        // 1允许任何域名使用
+//        corsConfiguration.addAllowedOrigin("*");
+//        // 2允许任何头
+//        corsConfiguration.addAllowedHeader("*");
+//        // 3允许任何方法（post、get等）
+//        corsConfiguration.addAllowedMethod("*");
+//        return corsConfiguration;
+//    }
 }
